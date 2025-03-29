6a246a4b5c59348b1aa6770216f093390ac1e05c
20nxskypqz <pom77449@gmail.com>
2025-03-29 16:08:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// สลับโหมด Day/Night
+function toggleMode() {
+    document.body.classList.toggle('night-mode');
+}
+
+// เปิด/ปิดเมนูสามขีด
+function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+}
