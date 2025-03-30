e3cb15cd210fc65c57ba7a52c0781b560a67d477
20nxskypqz <pom77449@gmail.com>
2025-03-30 14:16:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     dayNightToggle.addEventListener("click", () => {
         dayNightToggle.classList.toggle("night");
         const isNight = dayNightToggle.classList.contains("night");
-        document.body.style.backgroundColor = isNight ? "hsl(219, 30%, 12%)" : "hsl(219, 30%, 88%)";
-        document.body.style.color = isNight ? "hsl(219, 30%, 98%)" : "hsl(219, 30%, 20%)";
+        document.body.style.backgroundColor = isNight ? "var(--bg-dark)" : "var(--bg-light)";
+        document.body.style.color = isNight ? "var(--color-dark)" : "var(--color-light)";
     });
 });
